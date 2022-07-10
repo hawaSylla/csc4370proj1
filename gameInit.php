17be25129ea24a1172adf17b481af162850c0a34
kchib1 <kchib1@student.gsu.edu>
2022-07-10 19:37:41 -0400

    added pictures to person objects

--- gameInit.php
+++ gameInit.php
@@ -5,30 +5,30 @@
     global $mysteryPerson;
 
     //initating all people who will be used in guess who
-    $alex = new Person("alex", True, "black", "short", "straight", "brown", "small", False, True, True, False, False, False);
-    $alfred = new Person("alfred", True, "red", "long", "straight", "blue", "small", False, True, True, False, False, False);
-    $anita = new Person("anita", False, "blonde", "long", "straight", "blue", "small", False, False, False, False, True, False);
-    $anne = new Person("anne", False, "black", "short", "curly", "brown", "small", False, False, False, False, False, False);
-    $bernard = new Person("bernard", True, "brown", "short", "straight", "brown", "big", False, False, False, False, False, True);
-    $bill = new Person ("bill", True, "red", "bald", "straight", "brown", "small", False, True, False, True, True, False);
-    $charles = new Person ("charles", True, "blonde", "short", "straight", "brown", "small", False, True, True, False, False, False);
-    $claire = new Person ("claire", False, "red", "short", "curly", "brown", "small", True, False, False, False, False, True);
-    $david = new Person ("david", True, "blonde", "short", "straight", "brown", "small", False, True, False, True, False, False);
-    $eric = new Person ("eric", True, "blonde", "short", "straight", "brown", "small", False, False, False, False, False, True);
-    $frans = new Person ("frans", True, "red", "short", "curly", "brown", "small", False, False, False, False, False, False);
-    $george = new Person ("george", True, "white", "short", "straight", "brown", "small", False, False, False, False, False, True);
-    $herman = new Person ("herman", True, "red", "bald", "curly", "brown", "big", False, False, False, False, False, False);
-    $joe = new Person ("joe", True, "blonde", "short", "curly", "brown", "small", True, False, False, False, False, False);
-    $maria = new Person ("maria", False, "brown", "long", "wavy", "brown", "small", False, False, False, False, False, True);
-    $max = new Person ("max", True, "black", "short", "curly", "brown", "big", False, True, True, False, False, False);
-    $paul = new Person ("paul", True, "white", "short", "wavy", "brown", "small", True, False, False, False, False, False);
-    $peter = new Person ("peter", True, "white", "short", "straight", "blue", "big", False, False, False, False, False, False);
-    $philip = new Person ("philip", True, "black", "short", "curly", "brown", "small", False, True, False, True, True, False);
-    $richard = new Person ("richard", True, "brown", "bald", "straight", "brown", "small", False, True, True, True, False, False);
-    $robert = new Person ("robert", True, "brown", "short", "straight", "blue", "big", False, False, False, False, True, False);
-    $sam = new Person ("sam", True, "white", "bald", "straight", "brown", "small", True, False, False, False, False, False);
-    $susan = new Person ("susan", False, "white", "long", "straight", "brown", "small", False, False, False, False, True, False);
-    $tom = new Person ("tom", True, "black", "bald", "straight", "blue", "small", True, False, False, False, False, False);
+    $alex = new Person("alex", True, "black", "short", "straight", "brown", "small", False, True, True, False, False, False, "alex.jpg");
+    $alfred = new Person("alfred", True, "red", "long", "straight", "blue", "small", False, True, True, False, False, False, "alfred.jpg");
+    $anita = new Person("anita", False, "blonde", "long", "straight", "blue", "small", False, False, False, False, True, False, "anita.jpg");
+    $anne = new Person("anne", False, "black", "short", "curly", "brown", "small", False, False, False, False, False, False, "anne.jpg");
+    $bernard = new Person("bernard", True, "brown", "short", "straight", "brown", "big", False, False, False, False, False, True, "bernard.jpg");
+    $bill = new Person ("bill", True, "red", "bald", "straight", "brown", "small", False, True, False, True, True, False, "bill.jpg");
+    $charles = new Person ("charles", True, "blonde", "short", "straight", "brown", "small", False, True, True, False, False, False, "charles.jpg");
+    $claire = new Person ("claire", False, "red", "short", "curly", "brown", "small", True, False, False, False, False, True, "claire.jpg");
+    $david = new Person ("david", True, "blonde", "short", "straight", "brown", "small", False, True, False, True, False, False, "david.jpg");
+    $eric = new Person ("eric", True, "blonde", "short", "straight", "brown", "small", False, False, False, False, False, True, "eric.jpg");
+    $frans = new Person ("frans", True, "red", "short", "curly", "brown", "small", False, False, False, False, False, False, "frans.jpg");
+    $george = new Person ("george", True, "white", "short", "straight", "brown", "small", False, False, False, False, False, True, "george.jpg");
+    $herman = new Person ("herman", True, "red", "bald", "curly", "brown", "big", False, False, False, False, False, False, "Herman.jpg");
+    $joe = new Person ("joe", True, "blonde", "short", "curly", "brown", "small", True, False, False, False, False, False, "Joe.jpg");
+    $maria = new Person ("maria", False, "brown", "long", "wavy", "brown", "small", False, False, False, False, False, True, "maria.jpg");
+    $max = new Person ("max", True, "black", "short", "curly", "brown", "big", False, True, True, False, False, False, "max.jpg");
+    $paul = new Person ("paul", True, "white", "short", "wavy", "brown", "small", True, False, False, False, False, False, "paul.jpg");
+    $peter = new Person ("peter", True, "white", "short", "straight", "blue", "big", False, False, False, False, False, False, "peter.jpg");
+    $philip = new Person ("philip", True, "black", "short", "curly", "brown", "small", False, True, False, True, True, False, "philip.jpg");
+    $richard = new Person ("richard", True, "brown", "bald", "straight", "brown", "small", False, True, True, True, False, False, "Richard.jpg");
+    $robert = new Person ("robert", True, "brown", "short", "straight", "blue", "big", False, False, False, False, True, False, "Robert.jpg");
+    $sam = new Person ("sam", True, "white", "bald", "straight", "brown", "small", True, False, False, False, False, False, "Sam.jpg");
+    $susan = new Person ("susan", False, "white", "long", "straight", "brown", "small", False, False, False, False, True, False, "Susan.jpg");
+    $tom = new Person ("tom", True, "black", "bald", "straight", "blue", "small", True, False, False, False, False, False, "Tom.jpg");
 
     //array of all possible people
     $people = array($alex, $alfred, $anita, $anne, $bernard, $bill, $charles, $claire, $david, $eric, $frans, $george, $herman, $joe, $maria, $max, $paul, $peter, $philip, $richard, $robert, $sam, $susan, $tom);
