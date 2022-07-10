a9d291ffdd06febf1365a85c8b6505fad5e74bb8
kchib1 <kchib1@student.gsu.edu>
2022-07-10 19:49:30 -0400

    added nav bar to main.php

--- main.php
+++ main.php
@@ -5,6 +5,12 @@
     <link rel="stylesheet" href="startpage.css" />
   </head>
   <body>
+    <div class="topnav">
+        <a href="startpage.php"><img class="nav" src="imgs/guessWhoLogo.png" alt="guessWhoLogo" /></a>
+        <a href="guesswho.php">Play</a>
+        <a href="leaderboard.php">Leaderboards</a>
+        <a id="logout" href="starpage.php">Logout</a>
+    </div>
     <img src="imgs/guessWhoLogo.png" alt="guessWhoLogo" />
 
     <div class="start-page-buttons">
