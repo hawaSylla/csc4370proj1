04f725366844347fbe6a4d50d5c4ba591b83ceea
kchib1 <kchib1@student.gsu.edu>
2022-07-10 19:49:12 -0400

    added nav bar to main.php

--- guesswho.php
+++ guesswho.php
@@ -1,6 +1,10 @@
 <?php include("header.html"); ?>
 <?php include 'gameInit.php'; 
 
-    print $mysteryPerson->get_name() ?? null;   //debug: gets name of mystery person
+    //print $mysteryPerson->get_name() ?? null;   //debug: gets name of mystery person
 
     ?>
+
+    <div id="
+
+<?php include("footer.html"); ?>
