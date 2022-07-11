c0c54475558f307e49bfc758948a4eff57060299
kchib1 <kchib1@student.gsu.edu>
2022-07-10 22:51:01 -0400

    linked stylesheet

--- guesswho.php
+++ guesswho.php
@@ -2,6 +2,7 @@
     include("header.html");
     include("gameInit.php"); 
     session_start();
+    $strMP=$_SESSION['mysteryPerson']->get_pic() ?? null;
 ?>
 
     <table>
@@ -10,34 +11,34 @@
             <th></th>
             <th></th>
             <th></th>
-            <th><img src="<?php print $_SESSION['mysteryPerson']->get_pic() ?? null; ?>" alt="mystery person"></th>
+            <th><img src="imgs/backofcard.png" alt="mystery person"></th>
             <th></th>
             <th></th>
             <th></th>
             <th></th>
         </tr>
         <tr>
-            <td><img src="<?php print $_SESSION['randomPeople'][0]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][1]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][2]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][3]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][4]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][5]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][6]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][7]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][8]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][0]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][1]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][2]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][3]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][4]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][5]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][6]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][7]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][8]->get_pic() ?? null; ?>" alt="1"></td>
         </tr>
         <tr>
-            <td><img src="<?php print $_SESSION['randomPeople'][9]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][10]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][11]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][12]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][13]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][14]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][15]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][16]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][17]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img src="<?php print $_SESSION['randomPeople'][18]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][9]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][10]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][11]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][12]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][13]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][14]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][15]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][16]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][17]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][18]->get_pic() ?? null; ?>" alt="1"></td>
         </tr>
     </table>
 
