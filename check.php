ef2610c7f990ed88684f0f62bded99a396e57548
Hawa S <hmsylla10@gmail.com>
2022-07-11 01:02:19 -0400

    back button and formatting changes

--- check.php
+++ check.php
@@ -1,7 +1,7 @@
 <?php
-include ("gameInit.php");
+include("gameInit.php");
 session_start();
-$_SESSION['count'] = 0 ;
+$_SESSION['count'] = 0;
 $_SESSION['mysteryPerson'];
 //print_r($_SESSION);
 
@@ -14,60 +14,71 @@ $nGuess = $_POST['nose'];
 
 if (isset($_SESSION['mysteryPerson'])) {
     if ($userGuess == $_SESSION['mysteryPerson']->get_name() ?? null) {
-            echo "<center> you got it correct !</center>";
-            echo $_SESSION['count'];
+        echo "<center> you got it correct !</center>";
+        echo $_SESSION['count'];
+    } else if ($gGuess == $_SESSION['mysteryPerson']->get_isMale() ?? null) {
+        echo "<center> an attribute is correct, see if you can get the final guess!</center>";
+        $_SESSION['count']++;
+        echo "Count : ";
+        echo $_SESSION['count'];
+        // if (isset($_SERVER["HTTP_REFERER"])) {
+        //     header("Location: " . $_SERVER["HTTP_REFERER"]);
+        // }
+    } else if ($hGuess == $_SESSION['mysteryPerson']->get_hairColor() ?? null) {
+        echo "<center> an attribute is correct, see if you can get the final guess!</center>";
+        $_SESSION['count']++;
+        echo "Count : ";
+        echo $_SESSION['count'];
+        if (isset($_SERVER["HTTP_REFERER"])) {
+            header("Location: " . $_SERVER["HTTP_REFERER"]);
         }
-        else if($gGuess == $_SESSION['mysteryPerson']->get_isMale() ?? null){
-            echo "<center> an attribute is correct, see if you can get the final guess!</center>";
-            $_SESSION['count'] ++ ;
-            echo "Count : ";
-            echo $_SESSION['count'];
-            if (isset($_SERVER["HTTP_REFERER"])) {
-                header("Location: " . $_SERVER["HTTP_REFERER"]);
-            }
-        }
-        else if($hGuess == $_SESSION['mysteryPerson']->get_hairColor() ?? null){
-            echo "<center> an attribute is correct, see if you can get the final guess!</center>";
-            $_SESSION['count'] ++ ;
-            echo "Count : ";
-            echo $_SESSION['count'];
-            if (isset($_SERVER["HTTP_REFERER"])) {
-                header("Location: " . $_SERVER["HTTP_REFERER"]);
-            } 
-        }
-        else if($hLGuess == $_SESSION['mysteryPerson']->get_hairLength() ?? null){
-            echo "<center> an attribute is correct, see if you can get the final guess!</center>";
-            $_SESSION['count'] ++ ;
-            echo "Count : ";
-            echo $_SESSION['count'];
-            if (isset($_SERVER["HTTP_REFERER"])) {
-                header("Location: " . $_SERVER["HTTP_REFERER"]);
-            }
-        }
-        else if($eCGuess == $_SESSION['mysteryPerson']->get_eyeColor() ?? null){
-            echo "<center> an attribute is correct, see if you can get the final guess!</center>";
-            $_SESSION['count'] ++ ;
-            echo "Count : ";
-            echo $_SESSION['count'];
-            if (isset($_SERVER["HTTP_REFERER"])) {
-                header("Location: " . $_SERVER["HTTP_REFERER"]);
-            } 
-        }
-        else if($nGuess == $_SESSION['mysteryPerson']-> get_noseSize() ?? null){
-            echo "<center> an attribute is correct, see if you can get the final guess!</center>";
-            $_SESSION['count'] ++ ;
-            echo "Count : ";
-            echo $_SESSION['count'];
-            if (isset($_SERVER["HTTP_REFERER"])) {
-                header("Location: " . $_SERVER["HTTP_REFERER"]);
-            } 
-        }
-}
-
-
-else {
+    } else if ($hLGuess == $_SESSION['mysteryPerson']->get_hairLength() ?? null) {
+        echo "<center> an attribute is correct, see if you can get the final guess!</center>";
+        $_SESSION['count']++;
+        echo "Count : ";
+        echo $_SESSION['count'];
+        // if (isset($_SERVER["HTTP_REFERER"])) {
+        //     header("Location: " . $_SERVER["HTTP_REFERER"]);
+        // }
+    } else if ($eGuess == $_SESSION['mysteryPerson']->get_eyeColor() ?? null) {
+        echo "<center> an attribute is correct, see if you can get the final guess!</center>";
+        $_SESSION['count']++;
+        echo "Count : ";
+        echo $_SESSION['count'];
+        // if (isset($_SERVER["HTTP_REFERER"])) {
+        //     header("Location: " . $_SERVER["HTTP_REFERER"]);
+        // } 
+    } else if ($nGuess == $_SESSION['mysteryPerson']->get_noseSize() ?? null) {
+        echo "<center> an attribute is correct, see if you can get the final guess!</center>";
+        $_SESSION['count']++;
+        echo "Count : ";
+        echo $_SESSION['count'];
+        // if (isset($_SERVER["HTTP_REFERER"])) {
+        //     header("Location: " . $_SERVER["HTTP_REFERER"]);
+        // } 
+    }
+} else {
     echo "Uh oh";
 }
 
 
 ?>
+<html>
+
+<head>
+    <style>
+        .button {
+            transition-duration: 0.4s;
+        }
+
+        .button:hover {
+            background-color: #4CAF50;
+            /* Green */
+            color: white;
+        }
+    </style>
+</head>
+
+<button type="button">Go Back</button>
+
+</html>
--- guesswho.php
+++ guesswho.php
@@ -5,86 +5,87 @@ session_start();
 $profile = $_SESSION['mysteryPerson']->get_pic() ?? null;
 ?>
 <div>
-<form method="post" action="check.php">
-    <p>Guess The...</p>
-    <!-- <input type="radio" name=" Name" value="name"> Name<br>
+    <form method="post" action="check.php">
+        <p>Guess The...</p>
+        <!-- <input type="radio" name=" Name" value="name"> Name<br>
     <input type="radio" name=" Gender" value="name"> Gender<br>
     <input type="radio" name=" Hair Length" value="name"> Hair Length<br>
     <input type="radio" name=" Hair Color" value="name"> Hair Color<br>
     <input type="radio" name=" Eye Color" value="name"> Eye Color<br>
     <input type="radio" name=" Eye Size" value="name"> Eye Size <br> -->
 
-    <label for="Name">Guess The Name:</label>
-    <select id="Name" name="name">
-        <option value="null">null</option>
-        <option value="alex">alex</option>
-        <option value="alfred">alfred</option>
-        <option value="anit">anit</option>
-        <option value="anne">anne</option>
-        <option value="bernard">bernard</option>
-        <option value="bill">bill</option>
-        <option value="charles">charles</option>
-        <option value="claire">claire</option>
-        <option value="david">david</option>
-        <option value="eric">eric</option>
-        <option value="frans">frans</option>
-        <option value="george">george</option>
-        <option value="herman">herman</option>
-        <option value="joe">joe</option>
-        <option value="maria">maria</option>
-        <option value="max">max</option>
-        <option value="paul">paul</option>
-        <option value="peter">peter</option>
-        <option value="phillip">phillip</option>
-    </select>
+        <label for="Name">Guess The Name:</label>
+        <select id="Name" name="name">
+            <option value="null">null</option>
+            <option value="alex">alex</option>
+            <option value="alfred">alfred</option>
+            <option value="anita">anita</option>
+            <option value="anne">anne</option>
+            <option value="bernard">bernard</option>
+            <option value="bill">bill</option>
+            <option value="charles">charles</option>
+            <option value="claire">claire</option>
+            <option value="david">david</option>
+            <option value="eric">eric</option>
+            <option value="frans">frans</option>
+            <option value="george">george</option>
+            <option value="herman">herman</option>
+            <option value="joe">joe</option>
+            <option value="maria">maria</option>
+            <option value="max">max</option>
+            <option value="paul">paul</option>
+            <option value="peter">peter</option>
+            <option value="phillip">phillip</option>
+        </select>
 
-    <label for="Gender">Guess The Gender:</label>
-    <select id="Gender" name="Gender">
-        <option value=true>Male</option>
-        <option value=false>Female</option>
-        <option value=null>null</option>
-    </select>
+        <label for="Gender">Guess The Gender:</label>
+        <select id="Gender" name="Gender">
+            <option value=null>null</option>
+            <option value=true>Male</option>
+            <option value=false>Female</option>
+        </select>
 
-    <label for="Hair">Guess The Hair Color:</label>
-    <select id="HairColor" name="HairC">
-        <option value="Long">Long</option>
-        <option value="Short">Short</option>
-        <option value=null>null</option>
+        <label for="Hair">Guess The Hair Length:</label>
+        <select id="HairColor" name="HairC">
+            <option value=null>null</option>
+            <option value="Long">Long</option>
+            <option value="Short">Short</option>
 
-    </select>
+        </select>
 
-    <label for="Hair">Guess The Hair Length:</label>
-    <select id="HairLength" name="HairL">
-        <option value="black">black</option>
-        <option value="red">red</option>
-        <option value="blonde">blonde</option>
-        <option value="brown">brown</option>
-        <option value="bald">bald</option>
-        <option value="white">white</option>
-        <option value=null>null</option>
+        <label for="Hair">Guess The Hair Color:</label>
+        <select id="HairLength" name="HairL">
+            <option value=null>null</option>
+            <option value="black">black</option>
+            <option value="red">red</option>
+            <option value="blonde">blonde</option>
+            <option value="brown">brown</option>
+            <option value="bald">bald</option>
+            <option value="white">white</option>
 
         </select>
 
         <label for="Eye">Guess The Eye Color:</label>
-    <select id="EyeColor" name="EyeC">
-        <option value="blue">blue</option>
-        <option value="brown">brown</option>
-        <option value=null>null</option>
+        <select id="EyeColor" name="EyeC">
+            <option value=null>null</option>
+            <option value="blue">blue</option>
+            <option value="brown">brown</option>
 
         </select>
 
         <label for="nose">Guess The nose Size:</label>
-    <select id="noseSize" name="nose">
-        <option value="Large">Large</option>
-        <option value="Small">Small</option>
-        <option value=null>null</option>
+        <select id="noseSize" name="nose">
+            <option value=null>null</option>
+            <option value="Large">Large</option>
+            <option value="Small">Small</option>
+
 
         </select>
 
 
-    <input type="submit">
+        <input type="submit">
 
-<!--    
+        <!--    
     $form->addOption('length', 'long', 'long', '', '');
     $form->addOption('length', 'short', 'Option 2', '', '');
     $form->addOption('length', 'null', '-', '', 'selected');
@@ -99,65 +100,67 @@ $profile = $_SESSION['mysteryPerson']->get_pic() ?? null;
     $form->addSelect('eye_size', 'Eye Size', '');
     $form->addPlugin('formvalidation', '#fg-form', 'default', array('language' => 'en_US')); -->
 
-</form>
+    </form>
 </div>
 <div>
-<table>
-    <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th><div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img src="imgs/backofcard.png" alt="back">
-                    </div>
-                    <div class="flip-card-back">
-                        <img src="<?php print $profile ?>" alt="mystery person">
+    <table>
+        <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img src="imgs/backofcard.png" alt="back">
+                        </div>
+                        <div class="flip-card-back">
+                            <img src="<?php print $profile ?>" alt="mystery person">
+                        </div>
                     </div>
                 </div>
-            </div></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-    </tr>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td><img src="imgs/backsolid.png" alt="bg"></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td><img src="<?php print $_SESSION['randomPeople'][0]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][1]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][2]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][3]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][4]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][5]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][6]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][7]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][8]->get_pic() ?? null; ?>" alt="1"></td>
-    </tr>
-    <tr>
-        <td><img src="<?php print $_SESSION['randomPeople'][9]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][10]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][11]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][12]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][13]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][14]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][15]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][16]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][17]->get_pic() ?? null; ?>" alt="1"></td>
-        <td><img src="<?php print $_SESSION['randomPeople'][18]->get_pic() ?? null; ?>" alt="1"></td>
-    </tr>
-</table>
+            </th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><img src="imgs/backsolid.png" alt="bg"></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><img src="<?php print $_SESSION['randomPeople'][0]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][1]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][2]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][3]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][4]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][5]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][6]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][7]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][8]->get_pic() ?? null; ?>" alt="1"></td>
+        </tr>
+        <tr>
+            <td><img src="<?php print $_SESSION['randomPeople'][9]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][10]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][11]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][12]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][13]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][14]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][15]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][16]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][17]->get_pic() ?? null; ?>" alt="1"></td>
+            <td><img src="<?php print $_SESSION['randomPeople'][18]->get_pic() ?? null; ?>" alt="1"></td>
+        </tr>
+    </table>
 </div>
 
 
