606e2c81d62d36025a8331b34250acb41fcc6f29
kchib1 <kchib1@student.gsu.edu>
2022-07-11 00:30:14 -0400

    fixes

--- check.php
+++ check.php
@@ -1,22 +1,24 @@
 <?php
+include ("gameInit.php");
 session_start();
 $_SESSION['count'] = 0 ;
-$_SESSION['mysteryPerson'] = $_POST['mysteryPerson'];
+$_SESSION['mysteryPerson'];
+//print_r($_SESSION);
 
-$userGuess = $_GET['name'];
-$gGuess = $_GET['Gender'];
-$hGuess = $_GET['HairC'];
+$userGuess = $_POST['name'];
+$gGuess = $_POST['Gender'];
+$hGuess = $_POST['HairC'];
 
 if (isset($_SESSION['mysteryPerson'])) {
-    if ($userGuess = $mysteryPerson->get_name() ?? null) {
+    if ($userGuess == $_SESSION['mysteryPerson']->get_name() ?? null) {
             echo "<center> you got it correct !</center>";
             echo $_SESSION['count'];
         }
-        else if($mysteryPerson->get_isMale() ?? null){
+        else if($gGuess == $_SESSION['mysteryPerson']->get_isMale() ?? null){
             echo "<center> an attribute is correct, see if you can get the final guess!</center>";
             $_SESSION['count'] ++ ;
         }
-        else if($hGuess = $mysteryPerson->get_hairColor() ?? null){
+        else if($hGuess == $_SESSION['mysteryPerson']->get_hairColor() ?? null){
             echo "<center> an attribute is correct, see if you can get the final guess!</center>";
             $_SESSION['count'] ++ ;
         }
--- guesswho.php
+++ guesswho.php
@@ -4,7 +4,79 @@ include("gameInit.php");
 session_start();
 $profile = $_SESSION['mysteryPerson']->get_pic() ?? null;
 ?>
+<div>
+<form method="post" action="check.php">
+    <p>Guess The...</p>
+    <input type="radio" name=" Name" value="name"> Name<br>
+    <input type="radio" name=" Gender" value="name"> Gender<br>
+    <input type="radio" name=" Hair Length" value="name"> Hair Length<br>
+    <input type="radio" name=" Hair Color" value="name"> Hair Color<br>
+    <input type="radio" name=" Eye Color" value="name"> Eye Color<br>
+    <input type="radio" name=" Eye Size" value="name"> Eye Size <br>
 
+    <label for="Name">Guess The Name:</label>
+    <select id="Name" name="name">
+        <option value="null">null</option>
+        <option value="alex">alex</option>
+        <option value="alfred">alfred</option>
+        <option value="anit">anit</option>
+        <option value="anne">anne</option>
+        <option value="bernard">bernard</option>
+        <option value="bill">bill</option>
+        <option value="charles">charles</option>
+        <option value="claire">claire</option>
+        <option value="david">david</option>
+        <option value="eric">eric</option>
+        <option value="frans">frans</option>
+        <option value="george">george</option>
+        <option value="herman">herman</option>
+        <option value="joe">joe</option>
+        <option value="maria">maria</option>
+        <option value="max">max</option>
+        <option value="paul">paul</option>
+        <option value="peter">peter</option>
+        <option value="phillip">phillip</option>
+    </select>
+
+    <label for="Gender">Guess The Gender:</label>
+    <select id="Gender" name="Gender">
+        <option value=true>Male</option>
+        <option value=false>Female</option>
+        <option value=null>null</option>
+    </select>
+
+    <label for="Hair">Guess The Hair Color:</label>
+    <select id="HairColor" name="HairC">
+        <option value="black">black</option>
+        <option value="red">red</option>
+        <option value="blonde">blonde</option>
+        <option value="brown">brown</option>
+        <option value="bald">bald</option>
+        <option value="white">white</option>
+        <option value="null">null</option>
+
+    </select>
+
+    <input type="submit">
+
+<!--    
+    $form->addOption('length', 'long', 'long', '', '');
+    $form->addOption('length', 'short', 'Option 2', '', '');
+    $form->addOption('length', 'null', '-', '', 'selected');
+    $form->addSelect('length', 'Hair length', '');
+    $form->addOption('eye_color', 'brown', 'brown', '', '');
+    $form->addOption('eye_color', 'blue', 'Option 2', '', '');
+    $form->addOption('eye_color', 'null', '-', '', 'selected');
+    $form->addSelect('eye_color', 'Eye Color', '');
+    $form->addOption('eye_size', 'large', 'large', '', '');
+    $form->addOption('eye_size', 'small', 'small', '', '');
+    $form->addOption('eye_size', 'null', '-', '', 'selected');
+    $form->addSelect('eye_size', 'Eye Size', '');
+    $form->addPlugin('formvalidation', '#fg-form', 'default', array('language' => 'en_US')); -->
+
+</form>
+</div>
+<div>
 <table>
     <tr>
         <th></th>
@@ -12,15 +84,15 @@ $profile = $_SESSION['mysteryPerson']->get_pic() ?? null;
         <th></th>
         <th></th>
         <th><div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="imgs/backofcard.png" alt="back">
-    </div>
-    <div class="flip-card-back">
-      <img src="<?php print $profile ?>" alt="mystery person">
-    </div>
-  </div>
-</div></th>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="imgs/backofcard.png" alt="back">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="<?php print $profile ?>" alt="mystery person">
+                    </div>
+                </div>
+            </div></th>
         <th></th>
         <th></th>
         <th></th>
@@ -61,141 +133,8 @@ $profile = $_SESSION['mysteryPerson']->get_pic() ?? null;
         <td><img src="<?php print $_SESSION['randomPeople'][18]->get_pic() ?? null; ?>" alt="1"></td>
     </tr>
 </table>
+</div>
 
 
-
-<form method="post" ; action="check.php">
-    <p>Guess The...</p>
-    <input type="radio" name=" Name" value="name"> Name<br>
-    <input type="radio" name=" Gender" value="name"> Gender<br>
-    <input type="radio" name=" Hair Length" value="name"> Hair Length<br>
-    <input type="radio" name=" Hair Color" value="name"> Hair Color<br>
-    <input type="radio" name=" Eye Color" value="name"> Eye Color<br>
-    <input type="radio" name=" Eye Size" value="name"> Eye Size <br>
-
-    <label for="Name">Guess The Name:</label>
-    <select id="Name" name="name">
-        <option value="null">null</option>
-        <option value="alex">alex</option>
-        <option value="alfred">alfred</option>
-        <option value="anit">anit</option>
-        <option value="anne">anne</option>
-        <option value="bernard">bernard</option>
-        <option value="bill">bill</option>
-        <option value="charles">charles</option>
-        <option value="claire">claire</option>
-        <option value="david">david</option>
-        <option value="eric">eric</option>
-        <option value="frans">frans</option>
-        <option value="george">george</option>
-        <option value="herman">herman</option>
-        <option value="joe">joe</option>
-        <option value="maria">maria</option>
-        <option value="max">max</option>
-        <option value="paul">paul</option>
-        <option value="peter">peter</option>
-        <option value="phillip">phillip</option>
-    </select>
-
-    <label for="Gender">Guess The Gender:</label>
-    <select id="Gender" name="Gender">
-        <option value="true">Male</option>
-        <option value="false">Female</option>
-        <option value="null">null</option>
-    </select>
-
-    <label for="Hair">Guess The Hair Color:</label>
-    <select id="HairColor" name="HairC">
-        <option value="black">black</option>
-        <option value="red">red</option>
-        <option value="blonde">blonde</option>
-        <option value="brown">brown</option>
-        <option value="bald">bald</option>
-        <option value="white">white</option>
-        <option value="null">null</option>
-
-    </select>
-
-    <input type="submit">
-
-<!--    
-    $form->addOption('length', 'long', 'long', '', '');
-    $form->addOption('length', 'short', 'Option 2', '', '');
-    $form->addOption('length', 'null', '-', '', 'selected');
-    $form->addSelect('length', 'Hair length', '');
-    $form->addOption('eye_color', 'brown', 'brown', '', '');
-    $form->addOption('eye_color', 'blue', 'Option 2', '', '');
-    $form->addOption('eye_color', 'null', '-', '', 'selected');
-    $form->addSelect('eye_color', 'Eye Color', '');
-    $form->addOption('eye_size', 'large', 'large', '', '');
-    $form->addOption('eye_size', 'small', 'small', '', '');
-    $form->addOption('eye_size', 'null', '-', '', 'selected');
-    $form->addSelect('eye_size', 'Eye Size', '');
-    $form->addPlugin('formvalidation', '#fg-form', 'default', array('language' => 'en_US')); -->
-
-</form>
-<?php 
-    include("header.html");
-    include("gameInit.php"); 
-    session_start();
-    $strMP=$_SESSION['mysteryPerson']->get_pic() ?? null;
-?>
-
-    <table>
-        <tr>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th><div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="flip-card-front">
-                            <img src="imgs/backofcard.png" alt="backofcard">
-                        </div>
-                        <div class="flip-card-back">
-                            <img src="<?php print $strMP ?>" alt="mysteryPerson">
-                        </div>
-                    </div>
-                </div></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td><img src="imgs/backsolid.png" alt="background"></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][0]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][1]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][2]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][3]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][4]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][5]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][6]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][7]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][8]->get_pic() ?? null; ?>" alt="1"></td>
-        </tr>
-        <tr>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][9]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][10]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][11]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][12]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][13]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][14]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][15]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][16]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][17]->get_pic() ?? null; ?>" alt="1"></td>
-            <td><img class="profile" src="<?php print $_SESSION['randomPeople'][18]->get_pic() ?? null; ?>" alt="1"></td>
-        </tr>
-    </table>
 
 <?php include("footer.html"); ?>
